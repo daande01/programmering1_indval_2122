319ee3930d3965bb712707700c5eb4eb5a902f9a
daande1 <danielevertandersson>
2021-10-08 11:00:06 +0200

    new file

--- programmering1_indval_2122/src/uppgifter/Upp31.java
+++ programmering1_indval_2122/src/uppgifter/Upp31.java
@@ -1,5 +1,32 @@
 package uppgifter;
 
+import java.util.Scanner;
+
 public class Upp31 {
+
+	public static void main(String[] args) {
+
+		double [] d = new double[3];
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("ange ett tal");
+		d[0]=sc.nextDouble();
+
+		System.out.println("ange ett tal");
+		d[1]=sc.nextDouble();
+
+		System.out.println("ange ett tal");
+		d[2]=sc.nextDouble();
+
+		double summa = d[0]+d[1]+d[2];
+
+		double medelvärde= summa/3;
+
+		System.out.println("summa :" +summa+" medel:"+medelvärde);
+
+	}
+
+
 
 }
