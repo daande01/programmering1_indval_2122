e1fc2dcadcc6c9c7a97629a4f2b7e61c6bd8d280
daande1 <danielevertandersson>
2021-11-12 10:31:41 +0100

    new file

---
+++ programmering1_indval_2122/src/Selektion/Ovn72.java
@@ -0,0 +1,36 @@
+package Selektion;
+
+import javax.swing.JOptionPane;
+
+public class Ovn72 {
+
+	public static void main(String[] args) {
+
+		int tankvolym=50;
+		double prisPerLiter=9.5;
+
+
+		String kvarString=JOptionPane.showInputDialog("hur mycket bränsle kvar");
+
+		int bränslekvar=Integer.parseInt(kvarString);
+
+
+
+		if(bränslekvar >10) {
+
+			JOptionPane.showMessageDialog(null, "kör vidare");
+		}else {
+
+			int hurMycketKanFyllas=tankvolym-bränslekvar;
+			double kostnad=hurMycketKanFyllas*prisPerLiter;
+
+			JOptionPane.showMessageDialog(null, "du bör tank, du kan tanka"+hurMycketKanFyllas+"liter ,det kommer kosta "+kostnad);
+
+		}
+
+
+	}
+
+
+
+}
