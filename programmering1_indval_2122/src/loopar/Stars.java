6f4a6ee97a041aae9f76fadc7759beac7f52e5c3
daande1 <danielevertandersson>
2022-01-10 10:48:52 +0100

    new files

---
+++ programmering1_indval_2122/src/loopar/Stars.java
@@ -0,0 +1,21 @@
+package loopar;
+
+public class Stars {
+
+	public static void main(String[] args) {
+
+
+		for(int i =0;i<8;i++) {
+
+			for(int k=0;k<4;k++) {
+			System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+
+
+	}
+
+
+}
---
+++ programmering1_indval_2122/src/loopar/gånger.java
@@ -0,0 +1,20 @@
+package loopar;
+
+public class gånger {
+public static void main(String[] args) {
+
+
+	for (int i=0; i<10;i++) {
+
+		for(int k=0; k<10;k++) {
+
+		System.out.println(i+"*"+k+"="+i*k);
+		}
+	}
+
+
+
+
+
+}
+}
