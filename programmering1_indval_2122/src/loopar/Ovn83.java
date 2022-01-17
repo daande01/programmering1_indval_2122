b0a1c188adb78ec39e180ee9a60705180def680f
daande1 <danielevertandersson>
2022-01-17 09:51:35 +0100

    new file

---
+++ programmering1_indval_2122/src/loopar/Ovn83.java
@@ -0,0 +1,26 @@
+package loopar;
+
+public class Ovn83 {
+
+	public static void main(String[] args) {
+
+		System.out.println("m/s\tkm/h");
+
+		for(int i=5;i<=40;i=i+5) {
+
+			int kmPerH=(int)(i*3.6);
+
+			System.out.println(i+"\t"+kmPerH);
+
+
+		}
+
+
+
+
+
+
+	}
+
+
+}
