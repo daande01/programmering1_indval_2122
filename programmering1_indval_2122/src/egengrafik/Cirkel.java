6afd703e48aeb1700358fa00ea40605dc179b2ef
daande1 <danielevertandersson>
2022-03-25 13:34:51 +0100

    cirkel

--- programmering1_indval_2122/src/egengrafik/Cirkel.java
+++ programmering1_indval_2122/src/egengrafik/Cirkel.java
@@ -14,7 +14,10 @@ public class Cirkel extends JComponent {
 	private int y = 100; // instansvariabel
 	private int x = 100; // instansvariabel
 	private int xv=1;
-	private int yv=1;
+	private int yv=3;
+	private Color färg;
+
+	int g=2;
 
 	Timer t;
 
@@ -22,7 +25,7 @@ public class Cirkel extends JComponent {
 	public Cirkel() {
 
 		setPreferredSize(new Dimension(400,400));
-		t = new Timer(20, e -> {
+		t = new Timer(100, e -> {
 
 			update();
 			System.out.println("funkar");
@@ -36,22 +39,36 @@ public class Cirkel extends JComponent {
 	@Override
 	protected void paintComponent(Graphics g) {
 
+
+		färg=new Color((int)(Math.random()*255) ,(int)(Math.random()*255), 255);
 		super.paintComponent(g);
-		g.setColor(Color.blue);
+		g.setColor(färg);
 		g.fillOval(x, y, 50, 50);
 
 	}
 
 	public void update() {
 
+		yv=yv+g;
+
 		y=y+yv;
 		x=x+xv;
 
 		if((y+50)>=400) {
 
+			y=350;
+
 			yv=yv*-1;
 		}
 
+		if((x+50)>=400) {
+
+			xv=xv*-1;
+		}
+		if((y)<=0) {
+
+			yv=yv*-1;
+		}
 
 		repaint();
 	}
